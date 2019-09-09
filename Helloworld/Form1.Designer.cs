2684fd49ff6d069844505a2c6c4160fa8bab3444
Swindells <rc0037418@student.riversidecollege.ac.uk>
2019-09-09 15:15:27 +0100

    Add project files.

---
+++ Helloworld/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Helloworld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Hello = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Hello
+            // 
+            this.Hello.Location = new System.Drawing.Point(383, 161);
+            this.Hello.Name = "Hello";
+            this.Hello.Size = new System.Drawing.Size(156, 119);
+            this.Hello.TabIndex = 0;
+            this.Hello.Text = "Hello";
+            this.Hello.UseVisualStyleBackColor = true;
+            this.Hello.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Hello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Hello;
+    }
+}
+
